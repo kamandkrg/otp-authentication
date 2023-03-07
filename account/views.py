4545197bbd271ed15d42945b5f4f8d086fd62b5f
kamandkrg <kamandkargar@gmail.com>
2023-03-07 13:44:14 +0330

    make view for create user and verfy email

--- account/views.py
+++ account/views.py
@@ -1,3 +1,34 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, UpdateAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.views import APIView
 
-# Create your views here.
+from account.serializers import CreateUserSerializers, VerifyEmailSerializer
+
+User = get_user_model()
+
+
+class SingUpView(CreateAPIView):
+    permission_classes = (AllowAny, )
+    serializer_class = CreateUserSerializers
+    queryset = User.bojects.all()
+
+
+class VerifyEmailView(APIView):
+    permission_classes = (IsAuthenticated, )
+
+    def get_object(self):
+        try:
+            return User.objects.get(self.request.user.pk)
+        except User.DoesNotExist:
+            raise User
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+
