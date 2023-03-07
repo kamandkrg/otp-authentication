9886ca9c86be5096c98996e07e7866a2b96f4ebb
kamandkrg <kamandkargar@gmail.com>
2023-03-07 14:57:20 +0330

    make view for verify email

--- account/views.py
+++ account/views.py
@@ -2,9 +2,11 @@ from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from account.serializers import CreateUserSerializers, VerifyEmailSerializer
+from account.tasks import send_email
 
 User = get_user_model()
 
@@ -24,11 +26,17 @@ class VerifyEmailView(APIView):
         except User.DoesNotExist:
             raise User
 
-    def get(self, request):
-        pass
+    def get(self, request, *args, **kwargs):
+        serializer = VerifyEmailSerializer(request.user)
+        send_email.delay()
+        return Response(serializer.data)
 
     def post(self, request):
-        pass
+        serializer = VerifyEmailSerializer(request.User, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response('user is active')
+        return Response(serializer.errors)
 
 
 
