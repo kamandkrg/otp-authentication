444dbf2772a5d1bc15b830a5a16abd68125d16c6
kamandkrg <kamandkargar@gmail.com>
2023-03-07 14:06:47 +0330

    add task for send email

---
+++ account/tasks.py
@@ -0,0 +1,17 @@
+import random
+
+from celery import shared_task
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+
+User = get_user_model()
+
+
+@shared_task()
+def send_email(user):
+    token = random.randint(1000, 9999)
+    user.otp = token
+    email_from = settings.EMAIL_HOST_USER
+    send_mail('Verify Email', f'your token is: {token}', email_from, [user.email])
+
