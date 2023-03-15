a43a991fe2650c3b7903b605761c3d57e83f155a
kamandkrg <kamandkargar@gmail.com>
2023-03-15 12:25:11 +0330

    change admin

--- account/admin.py
+++ account/admin.py
@@ -10,19 +10,18 @@ User = get_user_model()
 class AccountAdmin(UserAdmin):
     form = UserChangeForm
     add_form = NewUserForm
-    list_display = ('id', 'email', 'phone', 'username', 'first_name', 'last_name',
-                    'is_active', 'verify_email',
-                    'is_staff', 'is_superuser')
+    list_display = ('id', 'email', 'phone', 'username', 'is_active', 'is_staff',
+                    'is_superuser')
     fieldsets = (
-        (None, {'fields': ('username', 'is_staff', 'is_superuser', 'verify_email', 'password', 'is_active')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'phone', 'email')}),
+        (None, {'fields': ('username', 'is_staff', 'is_superuser', 'password', 'is_active')}),
+        ('Personal info', {'fields': ('phone', 'email')}),
         ('Groups', {'fields': ('groups',)}),
         ('Permissions', {'fields': ('user_permissions',)}),
     )
     add_fieldsets = (
-        (None, {'fields': ('username', 'email', 'is_seller', 'is_staff', 'is_superuser', 'is_active', 'verify_email',
+        (None, {'fields': ('username', 'email', 'is_seller', 'is_staff', 'is_superuser', 'is_active',
                            'password1', 'password2')}),
-        ('Personal info', {'fields': ('first_name', 'last_name', 'phone', 'email')}),
+        ('Personal info', {'fields': ('phone', 'email')}),
         ('Groups', {'fields': ('groups',)}),
         ('Permissions', {'fields': ('user_permissions',)}),
     )
