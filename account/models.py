4019c367e1480cdb688017551e526075e71a1205
kamandkrg <kamandkargar@gmail.com>
2023-03-15 12:24:24 +0330

    change model to register with email and phone number

--- account/models.py
+++ account/models.py
@@ -5,17 +5,12 @@ from account.manager import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    username = models.CharField(max_length=32, unique=True, null=True, blank=True)
+    username = models.CharField(max_length=32, unique=True)
     phone = models.CharField(null=True, unique=True, blank=True, max_length=13)
-    email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=32, null=True, blank=True)
-    last_name = models.CharField(max_length=64, null=True, blank=True)
-    verify_email = models.BooleanField(default=False)
-    otp = models.IntegerField(null=True, blank=True)
-    national_code = models.PositiveBigIntegerField(null=True, blank=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
     objects = UserManager()
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'username'
